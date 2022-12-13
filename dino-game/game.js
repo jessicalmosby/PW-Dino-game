736f232c29df7706b9f0afecbed00cf28ec13ae7
Monika <moncobra@comcast.net>
2022-12-13 12:51:13 -0800

    dino sprites now change on click

--- dino-game/game.js
+++ dino-game/game.js
@@ -17,5 +17,15 @@ eggImg.addEventListener('click', async () => {
     const user = await getUser();
 
     await incrementAction(user.id);
-    await fetchActions(user.id);
+    //code is wet, look for alts//
+    const actionNum = await fetchActions(user.id);
+    if (actionNum[0].action_num === 1) {
+        eggImg.src = '../assets/egg-crack.gif';
+    }
+    if (actionNum[0].action_num === 2) {
+        eggImg.src = '../assets/egg-hatch.gif';
+    }
+    if (actionNum[0].action_num === 3) {
+        eggImg.src = '../assets/DinoSprite.gif';
+    }
 });
